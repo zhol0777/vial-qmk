cd78d04c9fae1ce05ae3ebef1de61f1ad191d51b
Zhol Internet <zhol@internet.keyboard>
2022-09-16 01:35:01 -0700

    add firmware for bkmf

---
+++ keyboards/zhol/bkmf/config.h
@@ -0,0 +1,27 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0x0777
+#define PRODUCT_ID      0x0001
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    zhol
+#define PRODUCT         bkmf
+
+/* key matrix size */
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 12
+
+#define MATRIX_ROW_PINS { B10, B11, B1, B2 }
+#define MATRIX_COL_PINS { B0, A7, A6, A5, A4, A2, B7, B6, B5, B4, B3, A15 }
+
+#define DIODE_DIRECTION COL2ROW
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+/* Use 1000hz polling */
+#define USB_POLLING_INTERVAL_MS 1
---
+++ keyboards/zhol/bkmf/readme.md
@@ -0,0 +1,12 @@
+# bkmf
+
+mkf studio bkmf
+
+* Keyboard Maintainer: [zhol#2829](https://github.com/zhol0777)
+* Hardware Supported: STM32F072CBT6 or APM32F072CBT6
+
+Make example for this keyboard (after setting up your build environment):
+
+    make zhol/bkmf:default
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
