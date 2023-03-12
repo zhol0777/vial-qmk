fcd71717ff17f91102439f9bbf8d9714de0889cf
Zhol Internet <zhol@internet.keyboard>
2023-03-11 16:16:22 -0800

    minilabaen

---
+++ keyboards/zhol/minilabaen/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x43, 0x36, 0x60, 0xD0, 0x6F, 0x9E, 0x5C, 0xE8}
+
+/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
+#define VIAL_INSECURE
