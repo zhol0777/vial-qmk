98090cbdc591966b77caacd481e3ff5ae05f4c73
Zhol Internet <zhol@internet.keyboard>
2023-04-03 02:09:26 -0700

    unfinished vintr board

---
+++ keyboards/zhol/vintboard/config.h
@@ -0,0 +1,27 @@
+// Copyright 2023 zhol (@zhol)
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#include "config_common.h"
+
+#define RGBLED_NUM ??
+#define RGB_DI_PIN ??
+
+/* This file is empty and unrequired */
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
---
+++ keyboards/zhol/vintboard/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x0B, 0x2A, 0xD7, 0xFC, 0xAD, 0xE8, 0xCB, 0x37}
+
+/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
+#define VIAL_INSECURE
