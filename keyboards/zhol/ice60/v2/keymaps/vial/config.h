fab6108f00f013a504a922683f42581470f63638
Zhol Internet <zhol@internet.keyboard>
2023-09-27 19:50:45 -0700

    ice60 v2

---
+++ keyboards/zhol/ice60/v2/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xC6, 0xC1, 0x20, 0x89, 0x64, 0xD9, 0x90, 0x20}
+
+/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
+// #define VIAL_INSECURE
