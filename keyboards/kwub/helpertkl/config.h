8c07b38f1f6a703f6072b441309cc901073a2249
Zhol Internet <zhol@internet.keyboard>
2023-11-23 19:36:11 -0800

    rebase

--- keyboards/kwub/helpertkl/config.h
+++ keyboards/kwub/helpertkl/config.h
@@ -16,14 +16,14 @@
 
 #pragma once
 
-#include "config_common.h"
+// #include "config_common.h"
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID    0x6B77  // Kwub
 #define PRODUCT_ID   0x0004
 #define DEVICE_VER   0x0001
-#define MANUFACTURER Kwub
-#define PRODUCT      HelperTKL
+#define MANUFACTURER "MKH Studio"
+#define PRODUCT      "Aduivare"
 
 /* key matrix size */
 #define MATRIX_ROWS  12
